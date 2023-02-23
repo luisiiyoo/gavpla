b0d83f1ea190da5b68fd99ae619e69c2bceceec0
Luis Gonzalez Guzman <luis.gonzalez@cw.groups360.com>
2023-02-23 17:51:20 -0600

    Configure linting

---
+++ .eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    "jest/globals": true // jest
+  },
+  extends: [
+    'standard',
+    'eslint:recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',  
+  },
+  plugins: [
+    '@typescript-eslint',  // For TypeScript.
+    'jest' // jest
+  ],
+  overrides: [ // Use `overrides` so ESLint can check both JS and TS files.
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: [
+        'plugin:@typescript-eslint/recommended', 
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',   
+      ],
+      parserOptions: {
+        tsconfigRootDir: __dirname,  
+        project: ['./tsconfig.json'],  
+      },
+    },
+    /* jest */
+    {
+      "files": ["test/**"],
+      "plugins": ["jest"],
+      "extends": ["plugin:jest/recommended"],
+      "rules": { "jest/prefer-expect-assertions": "off" }
+    }
+  ],
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    quotes: ["error", "double"],  // Use double quotes.
+    semi: ["error", "always"],  // Always add a semicolon at the end statements.
+    indent: ["error", 2],  // Indentation is two spaces.
+    "no-console": "error",  // Avoid using methods on `console` in the code.
+    /* jest */
+    "jest/no-disabled-tests": "warn",
+    "jest/no-focused-tests": "error",
+    "jest/no-identical-title": "error",
+    "jest/prefer-to-have-length": "warn",
+    "jest/valid-expect": "error"
+  }
+}
