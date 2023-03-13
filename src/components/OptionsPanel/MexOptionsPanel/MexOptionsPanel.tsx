97eeaa82b57a83c144f2ef2b75d9e1cd7aef5631
Luis Gonzalez Guzman <luis.gonzalez@cw.groups360.com>
2023-03-12 21:52:39 -0600

    WIP year choices

--- src/components/OptionsPanel/MexOptionsPanel/MexOptionsPanel.tsx
+++ src/components/OptionsPanel/MexOptionsPanel/MexOptionsPanel.tsx
@@ -32,24 +32,60 @@ const MexOptionsPanel = ({
     textAlign: 'center',
   };
   return (
-    <div className="MexOptionsPanel" style={style}>
-      <div style={{ ...style, justifyContent: 'space-between', width: '70%' }}>
-        <SelectOptionsPanel
-          label={'Year:'}
-          style={selectOptionsPanelStyle}
-          options={yearOptions}
-          selectionHandler={handleSelectYear}
-          selectionValue={selectedYear}
-        />
-        <SelectOptionsPanel
-          label={'State:'}
-          style={selectOptionsPanelStyle}
-          options={stateOptions}
-          selectionHandler={handleSelectState}
-          selectionValue={selectedState}
-        />
+    <>
+      <div className="MexOptionsPanel" style={style}>
+        <div
+          style={{ ...style, justifyContent: 'space-between', width: '70%' }}
+        >
+          <SelectOptionsPanel
+            label={'State:'}
+            style={selectOptionsPanelStyle}
+            options={stateOptions}
+            selectionHandler={handleSelectState}
+            selectionValue={selectedState}
+          />
+          <SelectOptionsPanel
+            label={'Year:'}
+            style={selectOptionsPanelStyle}
+            options={yearOptions}
+            selectionHandler={handleSelectYear}
+            selectionValue={selectedYear}
+          />
+        </div>
       </div>
-    </div>
+      <div
+        style={{
+          display: 'inline-block',
+          textAlign: 'center',
+          flexWrap: 'wrap',
+          width: '100%',
+        }}
+      >
+        <label>{'Year: '}</label>
+        {yearOptions.map((opt, key) => (
+          <YearChoice
+            label={opt}
+            key={key}
+            selectionHandler={handleSelectYear}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
+
+const YearChoice = ({ label, selectionHandler }) => {
+  return (
+    <button
+      className="YearChoice"
+      value={label}
+      onClick={(e) => {
+        e.preventDefault();
+        selectionHandler(e.target.value);
+      }}
+    >
+      {label}
+    </button>
   );
 };
 
