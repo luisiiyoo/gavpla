a53676e381da7d019ebb155fc24dd5809fd21cbf
Luis Gonzalez Guzman <luis.gonzalez@cw.groups360.com>
2023-03-28 21:37:31 -0600

    Updates

---
+++ precache-manifest.fb82781d97e778fc79d8bd345be97c80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0372763cb073b58c3a6fe7882d7b328",
+    "url": "/license-plates-gallery/index.html"
+  },
+  {
+    "revision": "7e88c2478cd146e94df1",
+    "url": "/license-plates-gallery/static/css/2.856795c6.chunk.css"
+  },
+  {
+    "revision": "8c7c873427ce5211abd4",
+    "url": "/license-plates-gallery/static/css/main.9fc2c36d.chunk.css"
+  },
+  {
+    "revision": "7e88c2478cd146e94df1",
+    "url": "/license-plates-gallery/static/js/2.00e6df90.chunk.js"
+  },
+  {
+    "revision": "0a5f30c26b11a957d3e84804aea4b657",
+    "url": "/license-plates-gallery/static/js/2.00e6df90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c7c873427ce5211abd4",
+    "url": "/license-plates-gallery/static/js/main.e6d90a79.chunk.js"
+  },
+  {
+    "revision": "a51c7ce283c8abbfbe6e",
+    "url": "/license-plates-gallery/static/js/runtime-main.23c93969.js"
+  }
+]);
