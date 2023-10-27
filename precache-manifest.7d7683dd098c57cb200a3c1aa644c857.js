63645cf4451e4340206fa4a33e6ea089c8974118
Luis Gonzalez Guzman <luis.gonzalez@cw.groups360.com>
2023-10-26 20:46:10 -0600

    Updates

---
+++ precache-manifest.7d7683dd098c57cb200a3c1aa644c857.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7efe0d71ad39a906954760e857a5891",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abd66410ba0a4da45145",
+    "url": "/static/css/2.e5487064.chunk.css"
+  },
+  {
+    "revision": "825d437ec78b9d0e0947",
+    "url": "/static/css/main.c0353057.chunk.css"
+  },
+  {
+    "revision": "abd66410ba0a4da45145",
+    "url": "/static/js/2.f1d86fc2.chunk.js"
+  },
+  {
+    "revision": "d404b667fc108cc115d2e323c684ba9b",
+    "url": "/static/js/2.f1d86fc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "825d437ec78b9d0e0947",
+    "url": "/static/js/main.33a254bb.chunk.js"
+  },
+  {
+    "revision": "6e63dc16dd5780518b24",
+    "url": "/static/js/runtime-main.ee8a6cf4.js"
+  },
+  {
+    "revision": "6ecfe4e6ea7d45c61299f985877dedfe",
+    "url": "/static/media/GAVPLA.6ecfe4e6.png"
+  },
+  {
+    "revision": "df568fd55103b4fcb323889bfb48e70f",
+    "url": "/static/media/mx.df568fd5.png"
+  },
+  {
+    "revision": "218f936fc192343d0485baaa35fe5e18",
+    "url": "/static/media/us.218f936f.png"
+  }
+]);
