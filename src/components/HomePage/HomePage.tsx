f906b4aa632a4a63a38f303903755f7837ce6dd5
Luis Gonzalez Guzman <luis.gonzalez@cw.groups360.com>
2026-05-05 14:34:59 -0600

    Remove resolver maps

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
 import { getTranslation } from 'src/language';
 import GAVPLA from 'src/images/GAVPLA.png';
@@ -10,24 +10,6 @@ const HomePage: React.FC = () => {
   } = useSelector((state) => state);
   const translation = getTranslation(languageCode, 'HomePage');
 
-  useEffect(() => {
-    // Add your script here
-    const script = document.createElement('script');
-    script.src =
-      '//rf.revolvermaps.com/0/0/6.js?i=5ebatgszveu&amp;m=7&amp;c=e63100&amp;cr1=ffffff&amp;f=arial&amp;l=0&amp;bv=90&amp;lx=-420&amp;ly=420&amp;hi=20&amp;he=7&amp;hc=a8ddff&amp;rs=80'; //'//rf.revolvermaps.com/0/0/8.js?i=5wm*****1mh&m=0&c=ff0000&cr1=ffffff&f=arial&l=33';
-    script.async = true;
-    // Load the script inside the specific div with the id "mapContainer"
-    const mapContainer = document.getElementById('mapContainer');
-    if (mapContainer) {
-      mapContainer.appendChild(script);
-    }
-    return () => {
-      // Remove the script when the component unmounts
-      if (mapContainer && mapContainer.contains(script)) {
-        mapContainer.removeChild(script);
-      }
-    };
-  }, []);
   return (
     <div className="HomePage" style={{}}>
       <>
@@ -40,11 +22,6 @@ const HomePage: React.FC = () => {
         <br />
         <br />
         <p>{translation['body']['p1']}</p>
-
-        <div id="mapContainer">
-          {/* The globe script will be loaded inside this div */}
-        </div>
-        <br />
         <p>{translation['body']['p2']}</p>
         <br />
 
